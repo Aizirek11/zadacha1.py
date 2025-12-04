25b553947cae823afc9463b5a01a1cb0093f5da3
Aizirek11 <aizireksovetbekova3@gmail.com>
2025-12-04 20:30:40 +0600

    < and > 100

---
+++ 4_12_2025.py
@@ -0,0 +1,13 @@
+numbers = [12, 34, 45, 67, 85, 97, 101, 10, 99, 1021, 89, 76, 67, 54]
+
+ 
+print("Числа меньше 100:")
+for num in numbers:
+    if num < 100:
+        print(num)
+
+ 
+print("Числа больше 100:")
+for num in numbers:
+    if num >= 100:
+        print(num)
--- zadacha1.py
+++ zadacha1.py
@@ -1,1 +1,15 @@
-print("hello")
+#n=(input("Напиши имя:"))
+#print("напиши задом на перед" , n[::-1])
+
+
+#def main(a ,b):
+#    result=a*b
+ #   print(result)
+
+
+#main(7,5)
+
+
+
+
+ 
